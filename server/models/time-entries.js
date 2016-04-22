b17f327ea3550a7fb84b788781502b721d9d10e3
mrlarssen <nmlarsse@broadpark.no>
2016-04-22 14:08:15 -0500

    Made harvest sync working with Trello

---
+++ server/models/time-entries.js
@@ -0,0 +1,49 @@
+//import { SimpleSchema } from 'meteor/aldeed:simple-schema';
+
+TimeEntries = new Mongo.Collection( 'timeEntries' );
+
+TimeEntrySchema = new SimpleSchema({
+  duration: {
+    type: Number,
+    decimal: true,
+    defaultValue: 0,
+    optional: true
+  },
+  date: {
+    type: Date,
+    optional: true
+  },
+  userId: {
+    type: SimpleSchema.RegEx.id,
+    optional: true
+  },
+  projectId: {
+    type: SimpleSchema.RegEx.id,
+    optional: true
+  },
+  taskId: {
+    type: SimpleSchema.RegEx.id,
+    optional: true
+  },
+  notes: {
+    type: String,
+    optional: true
+  }
+});
+
+TimeEntries.attachSchema( TimeEntrySchema );
+TimeEntries.attachBehaviour('timestampable');
+
+TimeEntries.helpers({
+  taskName (){
+    return Tasks.findOne(this.taskId).name;
+  },
+});
+
+TimeEntries.getTotalDuration = timeEntries => {
+  let durations = _.compact(_.pluck(timeEntries, 'duration'));
+
+  return durations.reduce((previous, current) => {
+    return previous + current;
+  }, 0);
+};
